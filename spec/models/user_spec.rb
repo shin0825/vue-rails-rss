3f1c0ab10dbd8906572c709668244ae40116e5bf
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-01-23 23:57:42 +0900

    railsチュートリアルに倣ってspecを1件追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "is valid with a name, account_id, and password_digest" do
+    user = User.new(
+      name: "nyanpoko",
+      account_id: "1",
+      password_digest: "hogefuga",
+    )
+    expect(user).to be_valid
+  end
+
   # TODO: ファクトリの作成
   it "アカウントidが重複している場合、エラーになること" do
 
