153ee281230ed66b2b16e0843f5331355f5eaa69
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-01-25 23:52:52 +0900

    仮のlinkのテスト追加 通過しない

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
   # TODO: ファクトリの作成
   it "正しいURLでない場合、エラーになること" do
-
+    link = Link.new(
+      url: "ttp://www.yahoo.co.jp"
+    )
+    expect(link).to_not be_valid
   end
 end
