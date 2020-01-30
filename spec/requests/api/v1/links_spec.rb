2625bf41ea540b6c5580ed4219f60f0e16290fb0
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-01-30 12:56:12 +0900

    add spec api link

--- spec/requests/api/v1/links_spec.rb
+++ spec/requests/api/v1/links_spec.rb
@@ -1,16 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe 'LinksAPI' do
-  it 'is get all Record of Links' do
-    FactoryBot.create_list(:link, 10)
+  describe 'GET /api/v1/links' do
+    it 'is get all Record of Links' do
+      FactoryBot.create_list(:link, 10)
 
-    get '/api/v1/links'
-    json = JSON.parse(response.body)
+      get api_v1_links_path
+      json = JSON.parse(response.body)
 
-    # リクエスト成功を表す200が返ってきたか確認する。
-    expect(response.status).to eq(200)
+      # リクエスト成功を表す200が返ってきたか確認する。
+      expect(response.status).to eq(200)
 
-    # 正しい数のデータが返されたか確認する。
-    expect(json.length).to eq(10)
+      # 正しい数のデータが返されたか確認する。
+      expect(json.length).to eq(10)
+    end
+  end
+
+  describe 'POST /api/v1/links' do
+    context 'Normal Parameter' do
+      before do
+        user = FactoryBot.create(:user)
+        sign_in user
+        @params = attributes_for(:link)
+      end
+
+      it 'should be 201 Created' do
+        post api_v1_links_path, params: @params
+        puts response
+        # expect(response).to be_success
+        expect(response.status).to eq(201)
+      end
+
+      it 'should be increment Link' do
+        expect { post api_v1_links_path, params: @params }.to change(Link, :count).by(1)
+      end
+    end
+
+    context 'Invalid Parameter' do
+      before do
+        @params = attributes_for(:link, url: '')
+      end
+
+      it 'should be 422 Unprocessable Entity' do
+        post api_v1_links_path, params: @params
+        # expect(response).not_to be_success
+        expect(response.status).to eq(422)
+      end
+
+      it 'should be not increment Link' do
+        expect { post api_v1_links_path, params: @params }.not_to change(Link, :count)
+      end
+    end
   end
 end
