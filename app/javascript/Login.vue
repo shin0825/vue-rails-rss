304a86c9553fbbea3833f222ba0b840f804027e0
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-02-16 23:59:37 +0900

    ログイン画面のデザイン調整

--- app/javascript/Login.vue
+++ app/javascript/Login.vue
@@ -1,45 +1,77 @@
 <template>
   <div>
-    <ul>
-      <li @click="tab = 1">Login</li>
-      <li @click="tab = 2">Register</li>
-    </ul>
     <div v-show="tab === 1">
-      <form @submit.prevent="login">
-        <label for="login-id">id</label>
-        <input type="text" id="login-id" class="original-input" v-model="loginForm.account_id">
-        <label for="login-password">Password</label>
-        <input type="password" id="login-password" class="original-input" v-model="loginForm.password">
-        <div>
-          <button type="submit" class="original-input">login</button>
-        </div>
-      </form>
+      <div class="tabpage">
+        <form @submit.prevent="login">
+          <ul>
+            <li><label for="login-id">id</label></li>
+            <li><input type="text" id="login-id" class="input original-input" v-model="loginForm.account_id"></li>
+            <li><label for="login-password">Password</label></li>
+            <li><input type="password" id="login-password" class="input original-input" v-model="loginForm.password"></li>
+            <li><button type="submit" class="submit original-input">login</button></li>
+          </ul>
+        </form>
+      </div>
     </div>
     <div v-show="tab === 2">
-      <form @submit.prevent="regist">
-        <div v-if="registErrors" class="errors">
-          <ul v-if="registErrors.account_id">
-            <li v-for="msg in registErrors.account_id" :key="msg">{{ msg }}</li>
+      <div class="tabpage">
+        <form @submit.prevent="regist">
+          <ul>
+            <li>
+              <div v-if="registErrors" class="errors">
+                <ul v-if="registErrors.account_id">
+                  <li v-for="msg in registErrors.account_id" :key="msg">{{ msg }}</li>
+                </ul>
+                <ul v-if="registErrors.password">
+                  <li v-for="msg in registErrors.password" :key="msg">{{ msg }}</li>
+                </ul>
+              </div>
+            </li>
+            <li><label for="register-id">id</label></li>
+            <li><input type="text" id="register-id" class="input original-input" v-model="registerForm.account_id"></li>
+            <li><label for="register-name">Name</label></li>
+            <li><input type="text" id="register-id" class="input original-input" v-model="registerForm.name"></li>
+            <li><label for="register-password">Password</label></li>
+            <li><input type="password" id="register-password" class="input original-input" v-model="registerForm.password"></li>
+            <li><label for="register-password_confirmation">Password(確認)</label></li>
+            <li><input type="password" id="register-password_confirmation" class="input original-input" v-model="registerForm.password_confirmation"></li>
+            <li><button type="submit" class="original-input">regist</button></li>
           </ul>
-          <ul v-if="registErrors.password">
-            <li v-for="msg in registErrors.password" :key="msg">{{ msg }}</li>
-          </ul>
-        </div>
-        <label for="register-id">id</label>
-        <input type="text" id="register-id" class="original-input" v-model="registerForm.account_id">
-        <label for="register-name">Name</label>
-        <input type="text" id="register-id" class="original-input" v-model="registerForm.name">
-        <label for="register-password">Password</label>
-        <input type="password" id="register-password" class="original-input" v-model="registerForm.password">
-        <label for="register-password_confirmation">Password(確認)</label>
-        <input type="password" id="register-password_confirmation" class="original-input" v-model="registerForm.password_confirmation">
-        <div>
-          <button type="submit" class="original-input">regist</button>
-        </div>
-      </form>
+        </form>
+      </div>
+    </div>
+    <div class="tab">
+      <ul>
+        <li v-show="tab === 2" @click="tab = 1">Login</li>
+        <li v-show="tab === 1" @click="tab = 2">Register</li>
+      </ul>
     </div>
   </div>
 </template>
+
+<style scoped>
+.tab {
+  display: flex;
+}
+.tabpage {
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.tabpage ul li {
+  text-align: center;
+  margin-top: 10px;
+}
+.input {
+  font-size: 2em;
+  width: 90%;
+}
+.submit {
+  font-size: 1.25em;
+  padding: 10px;
+}
+</style>
 
 <script>
 import axios from 'axios'
