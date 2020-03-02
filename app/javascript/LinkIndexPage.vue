a91a4f4b845314a01cc7cfe0ecb3e4c0654fb9d2
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-03-02 12:56:44 +0900

    add component LinkCard(仮)

--- app/javascript/LinkIndexPage.vue
+++ app/javascript/LinkIndexPage.vue
@@ -3,29 +3,9 @@
     <div class="container">
       <section class="section">
         <div class="columns is-variable is-12" v-for="link in links" :key="link.id">
-          <div class="column is-10 is-half">
+          <div class="cards column is-10 is-half">
             <div class="content is-medium">
-              <div class="card">
-                <div class="card-content">
-                  <p class="title">
-                    <a v-bind:href="link.url" target=”_blank” rel=”noopener”>
-                      {{ link.title }}
-                    </a>
-                  </p>
-                  <p class="subtitle">
-                    <router-link :to="{ name: 'LinkDetailPage', params: { id: link.id } }">
-                      {{ link.created_at | moment }}
-                    </router-link>
-                  </p>
-                </div>
-                <footer class="card-footer">
-                  <p class="card-footer-item">
-                    <a class="tweet" v-bind:href="link.tweet_url" target="_blank" rel=”noopener”>
-                      Tweet
-                    </a>
-                  </p>
-                </footer>
-              </div>
+              <LinkCard v-bind:link="link"/>
             </div>
           </div>
         </div>
@@ -34,6 +14,12 @@
   </div>
 </template>
 
+<style scoped>
+div.cards {
+  margin: 0 auto;
+}
+</style>
+
 <script>
 import moment from 'moment'
 import axios from 'axios'
@@ -41,8 +27,12 @@ axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest',
     'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 };
+import LinkCard from './components/LinkCard.vue'
 
 export default {
+  components: {
+    LinkCard
+  },
   computed: {
     links () {
       return this.$store.state.links.datas
@@ -63,32 +53,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-div.flexbox {
-  display: flex;
-}
-
-div.left-margin {
-  margin-left: 10px;
-}
-
-.tweet {
-  background-color: #3273dc;
-  color: #ffffff;
-  border-radius: 2px;
-  padding:2px 10px;
-  transition: all .3s;
-}
-
-.tweet:hover {
-  background-color: #ffffff;
-  color: #3273dc;
-}
-
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
---
+++ app/javascript/components/LinkCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <p class="title">
+        <a v-bind:href="link.url" target=”_blank” rel=”noopener”>
+          {{ link.title }}
+        </a>
+      </p>
+      <p class="subtitle">
+        <router-link :to="{ name: 'LinkDetailPage', params: { id: link.id } }">
+          {{ link.created_at | moment }}
+        </router-link>
+      </p>
+    </div>
+    <footer class="card-footer">
+      <p class="card-footer-item">
+        <a class="tweet" v-bind:href="link.tweet_url" target="_blank" rel=”noopener”>
+          Tweet
+        </a>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.tweet {
+  background-color: #3273dc;
+  color: #ffffff;
+  border-radius: 2px;
+  padding:2px 10px;
+  transition: all .3s;
+}
+
+.tweet:hover {
+  background-color: #ffffff;
+  color: #3273dc;
+}
+
+p {
+  font-size: 1.2em;
+  text-align: center;
+}
+</style>
+
+
+<script>
+export default {
+  props: {
+    'link': []
+  }
+}
+</script>
