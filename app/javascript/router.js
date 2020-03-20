1871f9371d1439161a445b6d5bbbab80b4eaa7a4
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-03-20 22:12:39 +0900

    update VueRouter fix scroll position

--- app/javascript/router.js
+++ app/javascript/router.js
@@ -54,7 +54,13 @@ const router = new VueRouter({
         }
       }
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 })
 
 export default router
